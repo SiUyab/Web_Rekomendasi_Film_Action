2f1bcd953f29a8938558ba36e5f84fe865d576e1
Bayu Satrio Bimantoro <93499217+SiUyab@users.noreply.github.com>
2022-01-10 13:40:55 +0700

    Add files via upload

---
+++ storage/framework/views/e3ef16f6d7ece83e63b41ab9c88497a3c8247335.php
@@ -0,0 +1,41 @@
+
+<?php $__env->startSection('content'); ?>
+<div class="container">
+      <div class="col-sm">
+        <div class="card" style="width: 60%;">
+            <img src="https://tamilrockerss.co.in/wp-content/uploads/2020/01/Venom-2-Tamil-Dubbed-TamilRockers.jpg" class="card-img-top" alt="gambar laptop">
+            <div class="card-body">
+              <h5 class="card-title">Venom: Let There Be Carnage</h5>
+              <p class="card-text">Sinopsis Venom: Let There Be Carnage mengisahkan Eddie Brock (Tom Hardy) yang kini harus beradaptasi menjalani hidup dengan Venom setelah berhasil selamat pada film pertamanya. Sementara itu, ada seorang pembunuh berantai bernama Cletus Kasady (Woody Harrelson) yang kabur dari penjara. Kondisi tersebut kemudian membahayakan Brock, karena Kasady juga punya symbiote yang bernama Carnage.
+
+              <div class="container">
+      <div class="col-sm">
+        <div class="card" style="width: 100%;">
+            <img src="https://th.bing.com/th/id/R.0668c73c8e5c350e930cbb9e3e1434f1?rik=FehUCAwyRKRPyA&riu=http%3a%2f%2fwww.blackfilm.com%2fread%2fwp-content%2fuploads%2f2018%2f05%2fMission-Impossible-Fallout-Poster-3.jpg&ehk=TzeGCCCiHIrTqU1RkCfa4ensWm%2fWGzZN50lBAIQxsy0%3d&risl=&pid=ImgRaw&r=0" class="card-img-top" alt="gambar laptop">
+            <div class="card-body">
+              <h5 class="card-title">Mission Impossible Fallout</h5>
+              <p class="card-text">Film ini berfokus pada alat pemusnah masal bernama Plutonium yang dihilangkan oleh Ethan Hunt saat negosiasi di Berlin. Kemudian ditugaskan kembali untuk mencari bom nuklir itu dengan bantuan Benji (Simon Pegg), Luther (Cing Rhames), dan August Walker (Cavill).
+Mereka pergi ke Perancis untuk bertemu dengan menyeludup senjata, White Widow untuk mendapatkan Plutonium. Ceritanya cukup rumit, dengan segala permasalahan antara mata-mata IMF, MI6, CIA dan Syndicate. Untuk kamu yang tidak mengikuti film ini dari franchise sebelumnya mungkin akan sedikit kebingungan. Tapi, hingga akhir ceritanya tetap berfokus pada nuklir tersebut.
+
+<div class="container">
+      <div class="col-sm">
+        <div class="card" style="width: 100%;">
+            <img src="https://th.bing.com/th/id/OIP.BUybjEzq3mp8QWT_MeBZZwHaK2?pid=ImgDet&rs=1" class="card-img-top" alt="gambar laptop">
+            <div class="card-body">
+              <h5 class="card-title">Free Guy</h5>
+              <p class="card-text">Guy (Ryan Reynolds) adalah seorang NPC di sebuah game bernama Free City. Ia diprogram sebagai seorang karyawan bank namun suatu ketika ia mulai melawan program tersebut setelah melihat seorang pemain perempuan. Guy kemudian melakukan berbagai cara untuk mendekati perempuan tersebut salah satunya dengan mengambil kaca mata yang biasa dipakai para pemain. Setelah mengambil kaca mata tersebut Guy mulai melihat dunianya sebagai seorang pemain, bukan lagi NPC. Guy kemudian mulai dekat dengan perempuan tersebut yang menyeretnya pada sebuah upaya untuk melawan perusahaan pengembang game Free City itu sendiri.
+
+              <div class="container">
+<div class="col-sm">
+    <div class="card" style="width: 100%;">
+            <img src="https://movies2all.com/wp-content/uploads/2021/05/Spider-Man-No-Way-Home-2048x1152.jpeg" class="card-img-top" alt="gambar laptop">
+            <div class="card-body">
+              <h5 class="card-title">Spider-Man: Now Way Home</h5>
+              <p class="card-text">Cerita film garapan sutradara Jon Watts ini memperlihatkan kehidupan Peter Parker setelah identitasnya sebagai Spider-Man dibuka oleh Mysterio (Jake Gyllenhaal). Publik terbagi dua; ada yang mendukung Mysterio dan percaya bahwa Spidey adalah 'pembunuh di balik wajah polos', dan yang lain percaya bahwa Spider-Man pahlawan. Semua diperparah dengan narasi J. Jonah Jameson (J.K. Simmons)--host acara TheDailyBugle.net--yang menempatkan Spider-Man sebagai villain.
+
+</p>
+            </div>
+          </div>
+      </div>    
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\Rekomendasi-Film-Action\resources\views/index.blade.php ENDPATH**/ ?>
