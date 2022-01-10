bade64250a6c864be10d550fc2589a4ded69a4e9
Bayu Satrio Bimantoro <93499217+SiUyab@users.noreply.github.com>
2022-01-10 13:38:59 +0700

    Add files via upload

---
+++ views/action.blade.php
@@ -0,0 +1,23 @@
+@extends('master')
+@section('content')
+<div class="container">
+      <div class="col-sm">
+        <div class="card" style="width: 60%;">
+            <img src="https://th.bing.com/th/id/OIP.daE-8dMCmXErfyWuUwErmgHaLM?pid=ImgDet&rs=1" class="card-img-top" alt="gambar laptop">
+            <div class="card-body">
+              <h5 class="card-title">Merah Putih</h5>
+              <p class="card-text">Merah Putih adalah film yang diproduksi oleh kolaborasi Media Desa Indonesia milik Hashim Djojohadikusumo (pengusaha dan adik dari Prabowo Subianto) dan rumah produksi film nasional Margate House milik Rob Allyn dan Jeremy Stewart. Latar cerita film ini diambil berdasarkan sejarah perjuangan kemerdekaan Indonesia pada tahun 1947 saat terjadinya peristiwa Agresi Militer Belanda I ke jantung pemerintahan Republik Indonesia di Jawa Tengah. Cerita Merah Putih berputar di sekawanan karakter fiktif yang menjalin persahabatan sebagai kadet dan selamat dari pembantaian oleh tentara Belanda. Mereka kemudian berperang sebagai tentara gerilya di pedalaman dengan diwarnai konflik karena perbedaan sifat, status sosial, etnis, budaya, dan agama.
+
+              <div class="container">
+      <div class="col-sm">
+        <div class="card" style="width: 100%;">
+            <img src="https://th.bing.com/th/id/OIP.DPBylj0p7itD57UYSAyF4wHaJD?pid=ImgDet&rs=1" class="card-img-top" alt="gambar laptop">
+            <div class="card-body">
+              <h5 class="card-title">Tarung Sarung</h5>
+              <p class="card-text">Tarung Sarung merupakan film terbaru yang mengangkat tema bela diri. Film action ini mengangkat sisi bela diri tradisional Tarung Sarung dengan dibalut elemen modern. Bercerita tentang Deni Ruso (Panji Zoni), anak dari keluarga terkaya di Jakarta. Kekuasaan dan harta bikin Deni semena-mena dan berkelahi. Suatu ketika, ia terlibat dalam perkelahian yang membuatnya hampir membunuh orang. Keluarganya mengirim Deni ke Makassar untuk memperbaiki diri. Di sana, ia bertemu dengan Tenri (Maizura), gadis yang menarik hatinya. Ia rela melakukan apapun, termasuk menjuarai kompetisi tarung sarung untuk mendapatkan hatinya.
+
+</p>
+            </div>
+          </div>
+      </div>    
+@endsection
